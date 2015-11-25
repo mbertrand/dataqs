a4f2343daf7582272c4bc78f33a0d2bfaab724e1
Matt Bertrand <matt@epidemico.com>
2015-11-25 09:30:35 -0500

    Send store to updatelayers command for performance improvement

--- dataqs/gdacs/tasks.py
+++ dataqs/gdacs/tasks.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import
 from celery import shared_task
 from dataqs.gdacs.gdacs import GDACSProcessor
 
+
 @shared_task()
 def gdacs_task():
     processor = GDACSProcessor()
